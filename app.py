5abc76fc0d8b6617010d975b2e748f6f6b67ab6d
Dksony8 <dipikashroff85@gmail.com>
2024-11-16 14:54:54 +0545

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,10 @@ def enhance_image():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
- 
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Default port 5000 hai
+    app.run(host="0.0.0.0", port=port)
+
         
